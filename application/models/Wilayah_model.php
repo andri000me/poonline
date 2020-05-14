d4e603f8fc68b96c22b11f3fe3b529980b8851f8
zulhan <56597805+zulhan97@users.noreply.github.com>
2020-05-15 01:07:05 +0700

    perbaikan wilayah_model

--- application/models/Wilayah_model.php
+++ application/models/Wilayah_model.php
@@ -1,12 +1,16 @@
 <?php
-class Wilayah_model extends CI_Model{
-    function get_all_provinsi(){
-        $query = $this->db->query("SELECT * FROM provinsi ORDER by id DESC");
-        return $query->result();
-    }
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-    function get_all_kota(){
-        $query = $this->db->query("SELECT * FROM kota ORDER by id DESC");
-        return $query->result();
+class Wilayah_model extends CI_Model{
+
+    public function get_all_provinsi(){
+        $this->db->order_by('nama_provinsi', 'asc');
+            return $this->db->get('provinsi')->result(); 
+        
+    }
+    public function get_all_kota(){
+    $this->db->order_by('nama_kota', 'asc');
+            $this->db->join('provinsi', 'kota.kota_id_provinsi = provinsi.id_provinsi');
+            return $this->db->get('kota')->result();
     }
 }
